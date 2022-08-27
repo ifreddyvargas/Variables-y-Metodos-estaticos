628f9f7763baee9c436f0749a05ff1b2880f99b3
ifreddyvargas <fr3do12099@gmail.com>
2022-08-26 12:28:05 -1200

    Practica numero 1 en clases

---
+++ Persona/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+//import java.util.*
+
+public class Titulo {
+
+    public static void main(String[titulo] args) {
+	         string nombre;
+	         string primerApellido;
+	         string segundoApellido;
+	         Date fechaNacimiento;
+	         string ci;
+    }
+}
